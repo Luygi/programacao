ccc3df721e1fe321f64d02e76a600e18c7cfe902
JETHER LUYGI DA SILVA ROCHA <04052616@sempreunama.com.br>
2019-08-27 20:53:53 -0300

    Initial commit

---
+++ src/com/unama/LetraC.java
@@ -0,0 +1,27 @@
+package com.unama;
+
+import java.util.Scanner;
+
+public class LetraC {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int quantidade;
+        double preco;
+        double total;
+        System.out.println("Informe a quantidade: ");
+        sc.nextInt() = quantidade;
+        System.out.println("Informe o preço do produto: ");
+        sc.nextDouble() = preco;
+        total = quantidade * preco;
+        sc.nextDouble() = total
+        System.out.println("O total a ser pago é " + total);
+
+
+
+
+
+
+
+    }
+}
